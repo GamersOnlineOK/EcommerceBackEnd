e7c35adb4620591b2c78435bde289379c8516bfd
Leonardo De Los Santos <leo_dls@hotmail.com>
2021-09-15 19:33:48 -0300

    Actualizacion de carrito cart/guardar busca y envia producto por ID

--- src/rutas/cart.route.js
+++ src/rutas/cart.route.js
@@ -4,11 +4,12 @@ const fs = require("fs");
 const { nextTick } = require('process');
 const { async } = require('regenerator-runtime');
 const FILE_PRODUCTOS = "productos.txt";
-const prod = require('../models/apicart');
+const cart = require('../models/apicart');
+const prod=require('../models/api')
 
 router.get('/', async (req, res) => {
 
-    const ListProd = await prod.find();
+    const ListProd = await cart.find();
     console.log(ListProd);
     res.json(ListProd)
 })
@@ -17,24 +18,26 @@ router.get('/', async (req, res) => {
 // METODOS GET
 // ==============LISTAR PRODUCTO===================
 router.get('/cart/listar', async (req, res) => {
-    const ListProd = await prod.find();
+    const ListProd = await cart.find();
     console.log(ListProd);
     res.json(ListProd)
 })
 // ==============MOSTRAR PRODUCTO===================
 router.get('/cart/listar/:id',async (req, res) => {
     const IdToListOne=req.params.id;
-    const oneProducto =await prod.findById(IdToListOne);
+    const oneProducto =await cart.findById(IdToListOne);
     res.json(oneProducto);
 
 })
 // METODOS POST
 router.post('/cart/guardar', async (req, res) => {
-    const { id,timestamp,product } = req.body;
-    const producto = new prod({ id,timestamp, product });
+    
+    const oneProduct =await prod.findById(req.body.id);    
+    const { id,timestamp } = req.body;
+    const producto = new cart({ id,timestamp, product:oneProduct });
     console.log(producto);
     await producto.save();
-    res.json({title:"tarea Guardada"})
+    res.json({title:"Carrito enviado"})
 })
 
 // METODOS PUT
@@ -42,7 +45,7 @@ router.put('/cart/actualizar/:id',async (req, res) => {
     const { id,timestamp,product } = req.body;
     const updateProduct={id,title, price, img };
     const idToUpdate= req.params.id;
-    await prod.findByIdAndUpdate(idToUpdate,updateProduct);
+    await cart.findByIdAndUpdate(idToUpdate,updateProduct);
     res.json(updateProduct);
 
 })
@@ -50,7 +53,7 @@ router.put('/cart/actualizar/:id',async (req, res) => {
 router.delete('/cart/eliminar/:id', async (req, res) => {
     const idToDelete=req.params.id;
     console.log(idToDelete);
-    await prod.findByIdAndRemove(idToDelete);
+    await cart.findByIdAndRemove(idToDelete);
     res.send("Eliminado")
 
 })
